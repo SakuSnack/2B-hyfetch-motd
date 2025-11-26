ac13edaa263b958af3215d5b87367f8380405478
Snackura <sakurahoshihime@gmail.com>
2025-11-26 15:03:24 +0100

    Easy update support

--- thats-so-random.sh
+++ thats-so-random.sh
@@ -8,6 +8,21 @@ else
 fi
 logoLocation="$dataLocation/logos"
 
+if [ ! -z $1 ] && [ $1 = "update" ]; then
+	{
+		mkdir -p $dataLocation
+		cd $dataLocation
+		git clone https://github.com/SakuSnack/2B-hyfetch-motd || exit 1
+		cd 2B-hyfetch-motd
+		chmod +x install.sh
+		THATS_SO_RANDOM_UPDATE=1 ./install.sh
+		unset THATS_SO_RANDOM_UPDATE
+		cd ..
+		rm -rf 2B-hyfetch-motd
+		exit 0
+	}
+fi
+
 randomFetch=()
 for logo in $logoLocation/*; do {
 	randomFetch+=("hyfetch --ascii-file $logo")
