fd2c1e7308605153b0410900adf010294ec8532e
Snackura <sakurahoshihime@gmail.com>
2026-01-01 03:12:02 +0100

    Update copyright

--- thats-so-stringreplace.sh
+++ thats-so-stringreplace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # thats-so-stringreplace.sh
-# Copyright (C) 2025 SakuSnack
+# Copyright (C) 2025-2026 SakuSnack
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
